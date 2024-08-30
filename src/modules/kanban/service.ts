2d48971567fdd5a805b36b83e2337ce1510dd422
Mighty683 <tomasz93szarek@gmail.com>
2024-08-30 10:38:29 +0200

    chore: refactor

--- src/modules/kanban/service.ts
+++ src/modules/kanban/service.ts
@@ -1,17 +1,21 @@
-import type { ProbabilityTable, TeamIterationResult } from './types';
+import type { ProbabilityArray, SimulationResult, TeamIterationResult } from './types';
 
 export function monteCarloTaskPrediction(
 	previousIterations: TeamIterationResult[],
 	remainingTasksCount: number,
 	numSimulations = 1_000_000
-): ProbabilityTable {
-	console.log(
-		'Running Monte Carlo simulation with',
-		numSimulations,
-		'simulations',
-		previousIterations,
-		remainingTasksCount
+): ProbabilityArray {
+	return createProbabilityTable(
+		runSimulation(numSimulations, previousIterations, remainingTasksCount),
+		numSimulations
 	);
+}
+
+export function runSimulation(
+	numSimulations: number,
+	previousIterations: number[],
+	remainingTasksCount: number
+): SimulationResult {
 	const results = [];
 
 	for (let i = 0; i < numSimulations; i++) {
@@ -19,8 +23,10 @@ export function monteCarloTaskPrediction(
 		let totalTime = 0;
 
 		while (tasksLeft > 0) {
+			const variabilityFactor = Math.random() * 0.2 + 0.9;
 			const randomIteration =
-				previousIterations[Math.floor(Math.random() * previousIterations.length)];
+				previousIterations[Math.floor(Math.random() * previousIterations.length)] *
+				variabilityFactor;
 			tasksLeft -= randomIteration;
 			totalTime += 1;
 		}
@@ -28,30 +34,60 @@ export function monteCarloTaskPrediction(
 		results.push(totalTime);
 	}
 
-	const probabilityTable: ProbabilityTable = [];
+	return results;
+}
 
-	results.forEach((time) => {
-		const existingProbability = probabilityTable.find((entry) => entry[0] === time);
-		if (existingProbability) {
-			existingProbability[1] += 1;
-		} else {
-			probabilityTable.push([time, 1]);
-		}
-	});
-	return (
-		probabilityTable
-			// Sort the probability table by time
-			.sort((a, b) => Number(a[0]) - Number(b[0]))
-			// Convert to percentage
-			.map((entry) => {
-				entry[1] = (entry[1] / numSimulations) * 100;
-				return entry;
-			})
-			// Calculate cumulative probability
-			.reduce<ProbabilityTable>((acc, entry, index) => {
-				entry[1] += acc[index - 1]?.[1] || 0;
-				acc.push(entry);
-				return acc;
-			}, [])
+export function createProbabilityTable(
+	simulationResults: number[],
+	numSimulations: number
+): ProbabilityArray {
+	const probabilityArray: ProbabilityArray = simulationResults.reduce(
+		reduceSimulationResultsToProbabilityArray,
+		[]
 	);
+
+	return (
+		probabilityArray
+			// Sort the probability table by time
+			.sort(sortByTime)
+			// Convert to percentage
+			.map(getMapToPercentage(numSimulations))
+			// Calculate cumulative probability
+			.reduce<ProbabilityArray>(reduceToCumulativeProbabilityArray, [])
+	);
+}
+function reduceSimulationResultsToProbabilityArray(
+	acc: ProbabilityArray,
+	entry: number
+): ProbabilityArray {
+	const existingProbability = acc.find((accEntry) => accEntry[0] === entry);
+	if (existingProbability) {
+		existingProbability[1] += 1;
+	} else {
+		acc.push([entry, 1]);
+	}
+	return acc;
+}
+
+function sortByTime(a: [number, number], b: [number, number]): number {
+	return a[0] - b[0];
+}
+
+function reduceToCumulativeProbabilityArray(
+	acc: ProbabilityArray,
+	entry: [number, number],
+	index: number
+): ProbabilityArray {
+	entry[1] += acc[index - 1]?.[1] || 0;
+	acc.push(entry);
+	return acc;
+}
+
+function getMapToPercentage(
+	numSimulations: number
+): (value: [number, number], index: number, array: [number, number][]) => [number, number] {
+	return (entry) => {
+		entry[1] = (entry[1] / numSimulations) * 100;
+		return entry;
+	};
 }
--- src/modules/kanban/types.ts
+++ src/modules/kanban/types.ts
@@ -1,4 +1,5 @@
 export type TeamIterationResult = number;
 export type NumberOfIterations = number;
 export type Probability = number;
-export type ProbabilityTable = [NumberOfIterations, Probability][];
+export type ProbabilityArray = [NumberOfIterations, Probability][];
+export type SimulationResult = NumberOfIterations[];
